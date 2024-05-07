eada263a6f57a54698b1b37eaf33ecd14ddaa676
Alex Chaconas <74623022+MrChaco007@users.noreply.github.com>
2024-05-07 17:16:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from "./Form";
 
 function App() {
 
-  const url = "https://tunr-back-end-fcb00f8e405b.herokuapp.com/";
+  const url = "https://tunr-rails-backend-75e3936479cd.herokuapp.com/";
   const [songs, setSongs] = React.useState([]);
   const emptySong = {
     title: "",
